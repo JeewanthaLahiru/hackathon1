6475c8abaa4beb1ced77107232a7f74e7ddfce5a
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2019-04-21 10:49:50 +0530

    hackothan1

---
+++ hackathon.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,sum;
+	printf("enter number 1 = ");
+	scanf("%d",&a);
+	printf("enter number 2 = ");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("\n summory= %d n\",sum);
+	return 0;
+}
